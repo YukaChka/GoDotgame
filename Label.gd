ee31a60937cb0dd022600f95c74a07815a9aec14
YukaChka <maxzerovol@gmail.com>
2023-03-22 11:40:27 +0300

    Сделал 2 видео

---
+++ Label.gd
@@ -0,0 +1,15 @@
+extends Label
+
+
+
+func _ready():
+	pass# Replace with function body.
+
+
+func _on_Button_pressed():
+	
+	if(text != "Hello suka!"):
+		text = "Hello suka!"
+	else:
+		text = "im script"
+	pass # Replace with function body.
