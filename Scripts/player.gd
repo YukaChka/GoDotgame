30f67ac62f53c48b33cdc2222d989c1b65c0a4cd
YukaChka <maxzerovol@gmail.com>
2023-03-23 20:33:15 +0300

    made 4

---
+++ Scripts/player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+var speed = 200
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	
+		
+	position.x = clamp(position.x,0,1000)
+	position.y = clamp(position.y,0,550)
+	
+	var velocity = Vector2()
+	if Input.is_action_pressed("Up"):
+		velocity.y -= speed
+	if Input.is_action_pressed("down"):
+		velocity.y += speed
+	if Input.is_action_pressed("left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("right"):
+		velocity.x += speed
+	move_and_slide(velocity)
+
+	
